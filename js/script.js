33e22ea5a3dbf27b7cc693d680a7329aeace9110
Felipe <cvfelipeoliveira@gmail.com>
2020-02-22 00:19:49 -0500

    call rest

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-var dataSetFavorites = [];
+var savedNews = [];
 
 $(document).ready(function() {
 	
-	// armazenamento das noticias em array para evitar calls desnecessarios
-	getDate();
-	getNews('sport');
-	getNews('business');
-	getNews('financial');
+	// add news into arrays 
+	now();
+	callRestApiNews('sport');
+	callRestApiNews('business');
+	callRestApiNews('financial');
 		
-	// escoder e exibir o conteudo conforme botoes de acao no menu
+	// show or hide zone of the news
 	$("#menu-home").on("click", function(e) {
-		getDate();
+		now();
 		$("#div-home").show();
 		$("#div-sport").hide();
 		$("#div-business").hide();
@@ -43,22 +43,23 @@ $(document).ready(function() {
 		var table = lineNews.split('-')[0];
 		var tr = lineNews.split('-')[1];
 		
-		var publishedAt = $("#dataTable-" + table)[0].rows[tr].cells[0].innerText;
-		var description = $("#dataTable-" + table)[0].rows[tr].cells[1].innerText;
+		var publishedAt = $("#dt-" + table)[0].rows[tr].cells[0].innerText;
+		var description = $("#dt-" + table)[0].rows[tr].cells[1].innerText;
 
-		dataSetFavorites.push([publishedAt, description, ""]);						
+		savedNews.push([publishedAt, description, ""]);						
 	});
 	
 	
 		
 });
 
-function getDate() {
-	$('#dataTable-home').html(new Date());	
+// get date
+function now() {
+	$('#dt-home').html(new Date());	
 }
 
-//metodo para obter as noticias do site news api atraves de um call GET
-function getNews(type) {
+// call rest to get news from api
+function callRestApiNews(type) {
 	var url = 'http://newsapi.org/v2/everything?q='+type+'&apiKey=d77b40b94e714053b0e20391cad1954b';
 	$.ajax({
 		url : url,
@@ -70,16 +71,16 @@ function getNews(type) {
 			var count = 0;
 			$.each(data.articles, function() {
 				news = {publishedAt : this.publishedAt, description : this.description};
-				dataSet.push([new Date(this.publishedAt).toLocaleDateString("en-IE"), this.description, "<a class='btn-fav' id='"+type+"-"+this.publishedAt+"' href='#'><i class='fas fa-star'></i></a>"]);				
+				dataSet.push([new Date(this.publishedAt), this.description, "<a class='btn-fav' id='"+type+"-"+this.publishedAt+"' href='#'><i class='fas fa-save'></i></a>"]);				
 			});
 			setDataTable(dataSet, type);			
 		})
 	});
 }
 
-// metodo para transformar o json em um datatable
+// json to datatable
 function setDataTable(dataSet, el) {
-	$('#dataTable-' + el).DataTable( {
+	$('#dt-' + el).DataTable( {
 		"searching": true,
 		"info": true,
 		"lengthChange": true,
@@ -87,8 +88,8 @@ function setDataTable(dataSet, el) {
 		data: dataSet,
 		columns: [
 			{ title: "Published" },
-			{ title: "Description" },
-			{ title: "Favorites" }
+			{ title: "News" },
+			{ title: "Action" }
 		]
 	});	
 }
